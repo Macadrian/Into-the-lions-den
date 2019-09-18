754a6c447ec2d0c1dfce98ffc75e52a0c381825e
Macadrian <adrianbautistagil@gmail.com>
2019-09-18 11:12:52 +0200

    Pruebas para el proyecto

---
+++ Assets/Scripts/GetWallsVertex.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetWallsVertex : MonoBehaviour
+{
+    CompositeCollider2D compositeCollider;
+    List<Vector2[]> paths = new List<Vector2[]>();
+
+    private void Awake()
+    {
+        compositeCollider = GetComponent<CompositeCollider2D>();
+
+        for (int i = 0; i < compositeCollider.pathCount; i++)
+        {
+            paths.Add(new Vector2[compositeCollider.GetPathPointCount(i)]);
+            compositeCollider.GetPath(i, paths[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (paths.Count != 0)
+        {
+            for (int indexPath = 0; indexPath < paths.Count; indexPath++)
+            {
+               
+                if (indexPath == 0) { Gizmos.color = Color.red; }
+                else { Gizmos.color = Color.grey; }
+
+                for (int indexVertex = 0; indexVertex < paths[indexPath].Length; indexVertex++)
+                {
+                    Gizmos.DrawSphere(paths[indexPath][indexVertex], 0.1f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Rigidbody2D rigidbody2D;
+
+    Vector2 movement;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        if (movement.x > 0) { } 
+        else if (movement.x < 0) { } 
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.position +=  (movement.normalized * speed * Time.deltaTime);
+    }
+}
