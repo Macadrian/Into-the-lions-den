d8ae6f3f5e9fe1389d35d57765203de0789695b4
Macadrian <adrianbautistagil@gmail.com>
2019-09-25 20:23:42 +0200

    Creando estructura de Herencia

---
+++ Assets/Scripts/Enemies/Fantasma.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fantasma : Vehicle
+{
+    [Min(0)] public float stoppingDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float remainingDistance = Vector3.Distance(target.position, transform.position);
+
+        if (remainingDistance > stoppingDistance)
+        {
+            rigidbody.AddForce(Seek(target.position), ForceMode2D.Force);
+        }
+    }
+
+    protected override Vector2 Seek(Vector3 targetPosition)
+    {
+        Vector2 steerForce = base.Seek(targetPosition);
+
+        return steerForce;
+    }
+}
--- Assets/Scripts/Enemies/Vehicle.cs
+++ Assets/Scripts/Enemies/Vehicle.cs
@@ -10,33 +10,44 @@ public class Vehicle : MonoBehaviour
 
     public Transform target;
 
-    Rigidbody2D rigidbody;
+    protected Rigidbody2D rigidbody;
 
     Vector3 velocity;
     Vector3 acceleration;
+
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual Vector2 Seek(Vector3 targetPosition)
     {
-        rigidbody.AddForce(Seek(this.target.position) ,ForceMode2D.Force);
-    }
-
-    private Vector2 Seek(Vector3 targetPosition)
-    {
-        Vector3 desiredVelocity = targetPosition - transform.position;
-        desiredVelocity = desiredVelocity.normalized * maxSpeed;
-
-        var desiredVelocity2D = new Vector2(desiredVelocity.x, desiredVelocity.y);
+        var desiredVelocity2D = DesiredVelocity(targetPosition);
 
         Vector2 steerForce = desiredVelocity2D - rigidbody.velocity;
 
         Vector2.ClampMagnitude(steerForce, maxForce);
 
         return steerForce;
+    }
+
+    protected virtual Vector2 Flee(Vector3 targetPosition)
+    {
+        var desiredVelocity2D = -DesiredVelocity(targetPosition);
+
+        Vector2 steerForce = desiredVelocity2D - rigidbody.velocity;
+
+        Vector2.ClampMagnitude(steerForce, maxForce);
+
+        return steerForce;
+    }
+
+    protected virtual Vector2 DesiredVelocity(Vector3 targetPosition)
+    {
+        Vector3 desiredVelocity = targetPosition - transform.position;
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+        return new Vector2(desiredVelocity.x, desiredVelocity.y);
     }
 }
