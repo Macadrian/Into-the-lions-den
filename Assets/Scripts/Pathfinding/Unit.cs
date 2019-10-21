3a89280a070153d1e20131da2d4b1a61576155b2
pablomorro <al361654@uji.es>
2019-10-21 11:35:11 +0200

    no gira

--- Assets/Scripts/Pathfinding/Unit.cs
+++ Assets/Scripts/Pathfinding/Unit.cs
@@ -65,6 +65,14 @@ public class Unit : MonoBehaviour
     {
         bool followingPath = true;
         Vector3 currentWayPoint = path[0];
+        visionSensor.LookAt(path[path.Length - 1]);
+
+        //Quaternion targetRotation = Quaternion.LookRotation(path[path.Length - 1] - transform.position);
+        //visionSensor.rotation = targetRotation;
+        //visionSensor.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+        /*Debug.Log(visionSensor.rotation.y);
+        Quaternion rotation = new Quaternion(0, 0, visionSensor.rotation.y, 0);
+        visionSensor.rotation = rotation;*/
 
         while (followingPath)
         {
