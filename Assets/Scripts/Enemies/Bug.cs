02921fc1d39c1f9893459d18538c752257074099
Macadrian <adrianbautistagil@gmail.com>
2019-10-26 20:02:09 +0200

    Details

--- Assets/Scripts/Enemies/Bug.cs
+++ Assets/Scripts/Enemies/Bug.cs
@@ -6,39 +6,36 @@ public class Bug : Vehicle
 {
 
     public LayerMask mask;
+    public float velovity = 5;
     public float distanciaMin = 3.5f;
-
-    public GameManager manager;
-    private Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = manager.playerTransform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distTarget = Vector3.Distance(target.transform.position, transform.position);
+        float distTarget = Vector3.Distance(GameManager.Instance.playerTransform.position, transform.position);
+        Vector2 steerForce = Flee(GameManager.Instance.playerTransform.position);
+
         if (distTarget <= distanciaMin)
         {
-            Vector2 steerForce = Flee(target.transform.position);
             var hit = Physics2D.Raycast(transform.position, steerForce, 1.5f, mask);
             if (hit)
             {
                 if (hit.normal.x == 1.0) steerForce.x *= -hit.normal.x;
                 else if (hit.normal.x == -1.0) steerForce.x *= hit.normal.x;
 
-                if (hit.normal.y == 1.0) steerForce.y *= -hit.normal.y;           
-                else if (hit.normal.y == -1.0) steerForce.y *= hit.normal.y;           
+                if (hit.normal.y == 1.0) steerForce.y *= -hit.normal.y;
+                else if (hit.normal.y == -1.0) steerForce.y *= hit.normal.y;
             }
 
             Debug.DrawRay(transform.position, steerForce.normalized, Color.blue);
             rigidbody.AddForce(steerForce, ForceMode2D.Force);
         }
-        else
-            rigidbody.velocity = Vector2.zero;
+
     }
 
     protected override Vector2 Flee(Vector3 targetPosition)
@@ -47,4 +44,15 @@ public class Bug : Vehicle
 
         return steerForce;
     }
+
+    /*protected Vector2 Seek()
+    {
+        //var desiredVelocity2D = ;
+
+        Vector2 steerForce = desiredVelocity2D - rigidbody.velocity;
+
+        steerForce = Vector2.ClampMagnitude(steerForce, maxForce);
+
+        return steerForce;
+    }*/
 }
